b676b561d5761a03fa2ae7c8a84198d3f090986a
Nikolai Guridov <125973969+guridovme@users.noreply.github.com>
2023-07-12 17:13:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,105 @@
-let post='';
-const titleInputNode=document.querySelector('.js-title-input');
+const posts = [];
+const TITLE_VALIDATION_LIMIT = 100;
+const TEXT_VALIDATION_LIMIT = 200;
+
+
+const postTitleInputNode=document.querySelector('.js-post-title-input');
+const postTextInputNode=document.querySelector('.js-post-text-input');
 const newPostBtnNode=document.querySelector('.js-new-post-btn');
 const postsNode=document.querySelector('.js-posts');
-
+const validationMessage=document.getElementById('validationMessage');
 
 newPostBtnNode.addEventListener('click', function(){
     const postFromUser = getPostFromUser();
-    setPost(postFromUser);
-    renderPost();
+    addPost(postFromUser);
+    renderPosts();
+    clearInputTitle(postTitleInputNode);
+    clearInputText(postTextInputNode);
 });
 
+postTitleInputNode.addEventListener("input", validation);
+
+postTextInputNode.addEventListener("input", validation);
+
+function validation () {
+    const titleLen = postTitleInputNode.value.length;
+    const textLen = postTextInputNode.value.length;
+        
+    if (titleLen > TITLE_VALIDATION_LIMIT){
+    validationMessage.innerText =` Длина заголовка не должна превышать ${TITLE_VALIDATION_LIMIT} символов`;
+    validationMessage.classList.remove("validationMessage_hidden");
+    return;
+    } 
+        
+    if (textLen > TEXT_VALIDATION_LIMIT){
+    validationMessage.innerText =` Длина текста не должна превышать ${TEXT_VALIDATION_LIMIT} символов`;
+    validationMessage.classList.remove("validationMessage_hidden");
+    return;
+    }
+    
+
+    validationMessage.classList.add("validationMessage_hidden");
+}
+
 function getPostFromUser(){
-    const post = titleInputNode.value;
-    return post;
+    const title = postTitleInputNode.value;
+    const text = postTextInputNode.value;
+
+    return {
+        title: title,
+        text: text
+    };
 };
 
-function setPost(newPost){
-    post=newPost;
+function addPost({ title, text}){
+    const currentDate = new Date();
+    var options = {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric'};
+
+    const dt = `${currentDate.toLocaleString("fr-CH", options)}`;
+
+    posts.push({
+    dt,
+    title,
+    text,
+    });
+
+
+
+
 };
 
-function renderPost(){
-    postsNode.innerText = post;
+function getPosts () {
+    return posts;
+};
+
+function renderPosts(){
+
+    const posts = getPosts ();
+
+    let postsHTML = '';
+
+    posts.forEach(post => {
+        postsHTML += `
+        <div class='post'>
+        <p class='post__date'>${post.dt}</p>
+        <p class='post__title'>${post.title}</p>
+        <p class='post__text'>${post.text}</p>
+        </div>
+        `;
+    });
+    
+    postsNode.innerHTML = postsHTML;
+};
+
+const clearInputTitle = function (input) {
+    postTitleInputNode.value="";
+};
+
+const clearInputText = function (input) {
+    postTextInputNode.value="";
 };
